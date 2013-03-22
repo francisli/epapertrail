4bce229f736c05f3f93bc920da1c232aad76fad0
Francis Li <mail@francisli.com>
2013-03-21 18:54:06 -0700

    Initial commit

---
+++ config/initializers/asset_sync.rb
@@ -0,0 +1,8 @@
+if defined?(AssetSync)
+  AssetSync.configure do |config|
+    config.fog_provider = 'AWS'
+    config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
+    config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+    config.fog_directory = ENV['AWS_S3_BUCKET']
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Epapertrail::Application.config.secret_token = '89292b097bde4173a587ad7550285695288e63d3a55d08cdad97007c3ce6d047aaf1d7ae55ba0dd6f373aa77c2693fc8bf57e9ea075b76a563804680606bbb98'
