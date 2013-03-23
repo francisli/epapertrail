c24bdd7a1da7cdec03772178d862d865a5f0fdfc
Francis Li <mail@francisli.com>
2013-03-23 12:24:23 -0700

    Fixed GeoIP initializer so doesn't fail on precompile during deployment

--- config/initializers/geoip.rb
+++ config/initializers/geoip.rb
@@ -1,9 +1,10 @@
 # Add an initialized singleton instance with accessor to the GeoIP class
 #
 class GeoIP
-  @@singleton = GeoIP.new("#{Rails.root}/public/GeoLiteCity.dat")
+  @@singleton = nil
   
   def self.lookup
+    @@singleton = GeoIP.new("#{Rails.root}/public/GeoLiteCity.dat") if @@singleton.nil?
     @@singleton
   end
   
