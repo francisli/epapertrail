072ccca048f7b7179019de020a0f9e84e4735504
Francis Li <mail@francisli.com>
2014-03-15 09:26:46 -0700

    Improved portrait layout, portrait images from sunlight labs, css grayscale filter on portraits

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
   def portrait_image_url(bioguide_id)
-    "//#{ENV['AWS_S3_BUCKET']}.s3.amazonaws.com/portraits/#{bioguide_id}.jpg"
+    "http://theunitedstates.io/images/congress/225x275/#{bioguide_id}.jpg"
   end
   
   def similarity_label(percentage)
